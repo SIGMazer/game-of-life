b103b4857fc3261c9baf520d6a97f2ced56446cc
SIGMazer <mazinasd7@gmail.com>
2024-01-02 16:12:14 +0200

    port the ui to raylib and and Brain's brain celluar automata
    Former-commit-id: eaef6807f82bca5705bba17ef78b8512aaa34499

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,54 @@
 use rand::Rng;
 use ncurses::*;
 use std::io::Write;
+use raylib::prelude::*;
 
-const HEIGHT: usize = 480;
-const WIDTH : usize = 640;
-const RULES: [[i32; 9]; 2] = [[0, 0, 0, 1, 0, 0, 0, 0, 0], 
-                              [0, 0, 1, 1, 0, 0, 0, 0, 0]];
+const HEIGHT: usize = 256;
+const WIDTH : usize = 256;
+use State::{Dead, Alive, Dying};
+const GOL: [[State; 9]; 2] = [[Dead, Dead, Dead, Alive, Dead, Dead, Dead, Dead, Dead], 
+                              [Dead, Dead, Alive, Alive, Dead, Dead, Dead, Dead, Dead]];
 
-fn print_board(board: &Vec<[i32; WIDTH]>) {
+//brain's brain
+const BB: [[State; 9]; 3] = [[Dead, Dead, Alive, Dead, Dead, Dead, Dead, Dead, Dead], 
+                             [Dying, Dying, Dying, Dying, Dying, Dying, Dying, Dying, Dying],
+                             [Dead, Dead, Dead, Dead, Dead, Dead, Dead, Dead, Dead]];
+                           
+                           
+
+#[derive(Copy, Clone, PartialEq, Debug)]
+enum State{
+    Dead,
+    Alive,
+    Dying,
+}
+
+#[derive(Copy, Clone, PartialEq, Debug)]
+enum Mode {
+    GOL,
+    BB,
+    
+}
+
+impl State {
+    fn as_usize(&self) -> usize {
+        match self {
+            State::Dead => 0,
+            State::Alive => 1,
+            State::Dying => 2,
+        }
+    }
+}
+
+fn _print_board(board: &Vec<[State; WIDTH]>) {
     for i in 0..HEIGHT {
         for j in 0..WIDTH {
-            if board[i][j] == 0 {
+            if board[i][j] == State::Dead {
                 addstr("-");
-            } else{
+            } if board[i][j] == State::Alive {
                 addstr("#");
+            } if board[i][j] == State::Dying {
+                addstr("X");
             }
 
         }
@@ -21,20 +56,20 @@ fn print_board(board: &Vec<[i32; WIDTH]>) {
     }
 }
 
-fn fill_random_board(board: &mut Vec<[i32; WIDTH]>){
+fn fill_random_board(board: &mut Vec<[State; WIDTH]>){
     let mut rng = rand::thread_rng();
     for i in 0..HEIGHT {
         for j in 0..WIDTH {
             let a: u32 = rng.gen();
             if a % 2 == 0{ 
-                board[i][j] = 1;
+                board[i][j] = State::Alive;
             }
 
         }
     }
 }
 
-fn count_neighbours(board: &Vec<[i32; WIDTH]>, i: usize, j: usize) -> usize {
+fn count_neighbours(board: &Vec<[State; WIDTH]>, i: usize, j: usize) -> usize {
     let mut count = 0;
     let i1 = if i == 0 { 0 } else { i-1 };
     let i2 = if i == HEIGHT-1 { HEIGHT } else { i+2 };
@@ -45,16 +80,16 @@ fn count_neighbours(board: &Vec<[i32; WIDTH]>, i: usize, j: usize) -> usize {
             if x == i && y == j {
                 continue;
             }
-            count += board[x][y] as usize;
+            count += if board[x][y] == State::Alive { 1 } else { 0 };
         }
     }
     count
 }
 
-fn _count_dump(board: &Vec<[i32; WIDTH]>) {
+fn _count_dump(board: &Vec<[State; WIDTH]>) {
     for i in 0..HEIGHT {
         for j in 0..WIDTH {
-            if board[i][j] == 1 {
+            if board[i][j] == State::Alive {
             let count = count_neighbours(&board, i, j);
             println!("({}, {}): {}", i, j, count);
 
@@ -64,25 +99,29 @@ fn _count_dump(board: &Vec<[i32; WIDTH]>) {
 }
 
 
-fn play(board: &mut Vec<[i32; WIDTH]>) -> Vec<[i32; WIDTH]> {
-    let mut new_board = vec![[0; WIDTH]; HEIGHT]; 
+fn play(board: &mut Vec<[State; WIDTH]>, mode: Mode) -> Vec<[State; WIDTH]> {
+    let mut new_board = vec![[Dead; WIDTH]; HEIGHT]; 
     for i in 0..HEIGHT {
         for j in 0..WIDTH {
             let count = count_neighbours(&board, i, j);
-            new_board[i][j] = RULES[board[i][j] as usize][count];
+            let idx = board[i][j].as_usize();
+            if mode == Mode::GOL {
+                new_board[i][j] = GOL[idx][count];
+            } else if mode == Mode::BB {
+                new_board[i][j] = BB[idx][count];
+            }
         }
     }
     new_board
 }
 
-fn save_frame_as_ppm(board: &Vec<[i32; WIDTH]>, offset: usize){
+fn _save_frame_as_ppm(board: &Vec<[i32; WIDTH]>, offset: usize){
     let filename = format!("data/frame{}.ppm", offset);
     let mut file = std::fs::File::create(filename).unwrap();
     let header = format!("P3\n{} {}\n255\n", WIDTH, HEIGHT);
     file.write_all(header.as_bytes()).unwrap();
     for i in 0..HEIGHT {
-        for j in 0..WIDTH {
-            let color = if board[i][j] == 1 { "67 118 108" } else { "255 255 255" };
+        for j in 0..WIDTH { let color = if board[i][j] == 1 { "67 118 108" } else { "255 255 255" };
             let line = format!("{}\n", color);
             file.write_all(line.as_bytes()).unwrap();
         }
@@ -90,37 +129,47 @@ fn save_frame_as_ppm(board: &Vec<[i32; WIDTH]>, offset: usize){
 }
 
 fn main() {
-    let mut board = vec![[0; WIDTH]; HEIGHT]; 
+    let mut board = vec![[State::Dead; WIDTH]; HEIGHT]; 
     fill_random_board(&mut board);
 
-    initscr();
+    let (mut rl, thread) = raylib::init()
+        .size(1920, 1080)
+        .title("Game of Life")
+        .build();
+    let mut isplay = false;
+    let mut mode = Mode::BB;
+    while !rl.window_should_close() {
+        let mut d = rl.begin_drawing(&thread);
+        let color = Color::new(18, 18, 18, 18);
+        d.clear_background(color);
+        for i in 0..HEIGHT {
+            for j in 0..WIDTH {
+                let color = if board[i][j] == Alive { Color::new(22, 255, 0, 255) }
+                    else if board[i][j] == Dying { Color::new(15, 98, 146, 255) }
+                    else {color };
 
-    print_board(&board);
-    refresh();
-
-    loop {
-        match getch() as u8 as char {
-            'q' => break,
-            'r' => {
-                clear();
-                board = play(&mut board);
-                print_board(&board);
-                refresh();
-            },
-            'p' => {
-                let mut _offset = 0;
-                loop {
-                    clear();
-                    board = play(&mut board); print_board(&board);
-                    save_frame_as_ppm(&board, _offset);
-                    _offset += 1;
-                    refresh();
-                    std::thread::sleep(std::time::Duration::from_millis(50));
-                }
-            },
-            _ => {}
+                d.draw_rectangle((j*8) as i32, (i*4) as i32, 3, 3, color);
+            }
+        }
+        if isplay {
+            board = play(&mut board, mode);
+        }
+        std::thread::sleep(std::time::Duration::from_millis(50));
+        if d.is_key_pressed(raylib::consts::KeyboardKey::KEY_Q) {
+            break;
+        }
+        if d.is_key_pressed(raylib::consts::KeyboardKey::KEY_R) {
+            fill_random_board(&mut board);
+        }
+        if d.is_key_pressed(raylib::consts::KeyboardKey::KEY_SPACE) {
+            isplay = !isplay; 
+        }
+        if d.is_key_pressed(raylib::consts::KeyboardKey::KEY_M) {
+            mode = match mode {
+                Mode::GOL => Mode::BB,
+                Mode::BB => Mode::GOL,
+            }
         }
     }
-    endwin();
 }
 
